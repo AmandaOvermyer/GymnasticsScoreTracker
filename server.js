9933a7dd568cd51f26810783a20c1bf55493b899
Amanda Overmyer <aovermyerphoto@gmail.com>
2017-03-10 14:31:07 +0000

    added cors middleware

--- server.js
+++ server.js
@@ -10,7 +10,24 @@ var jsonParser = bodyParser.json();
 
 var app = express();
 app.use(express.static('public'));
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.post('/gymnast', jsonParser, function(request, response){
 
 		Gymnast.create({name:request.body.name, age:request.body.age, gender:request.body.gender, level:request.body.level}, function(err, gymnast){
