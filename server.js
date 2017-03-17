ed945367c13094e63bad4d4b7c4115f356fdfc6b
Amanda Overmyer <aovermyerphoto@gmail.com>
2017-03-17 14:15:06 +0000

    test origin

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    /*res.setHeader('Access-Control-Allow-Origin', '*');*/
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
